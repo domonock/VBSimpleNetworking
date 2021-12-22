929ac9fa636bb669e0f0bb57ce8a72671cb6949c
Volodymyr Babych <volodymyr.babych@proxet.com>
2021-12-22 20:05:07 +0200

    Some updates

--- Sources/VBSimpleNetworking/VBSimpleNetworking.swift
+++ Sources/VBSimpleNetworking/VBSimpleNetworking.swift
@@ -1,4 +1,4 @@
-public struct VBSimpleNetworking {
+public class VBSimpleNetworking {
     
     public static func getDataFrom<T: Decodable>(url: String, dataType: T.Type) -> T {
         return VBServerManager.getDataFrom(url: url, dataType: dataType)
